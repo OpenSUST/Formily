39cfa67c8d8da175c665d4aef8d8d8e16ed32230
Shirasawa <764798966@qq.com>
2022-07-17 00:26:14 +0800

    更新模板

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react'
 
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
+import Autocomplete from '@mui/material/Autocomplete'
 import Typography from '@mui/material/Typography'
 import ListItemText from '@mui/material/ListItemText'
-import Avatar from '@mui/material/Avatar'
+import Checkbox from '@mui/material/Checkbox'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import Card from '@mui/material/Card'
 import Chip from '@mui/material/Chip'
 import Fab from '@mui/material/Fab'
 import TextField from '@mui/material/TextField'
+import CircularProgress from '@mui/material/CircularProgress'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import Container from '@mui/material/Container'
+import AddIcon from '@mui/icons-material/Add'
 import DeleteIcon from '@mui/icons-material/Delete'
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@mui/icons-material/CheckBox'
 import { typeNameMap } from './fields'
 import { openDialog } from './EnsureDialog'
 import { CircularLoading } from './Loading'
@@ -28,11 +32,21 @@ import { useQuery, useApolloClient } from '@apollo/client'
 import { useSnackbar } from 'notistack'
 import { useParams } from 'react-router-dom'
 
-const data2 = [{ title: '字段a', id: 'a' }, { title: '字段B', type: 'number', id: 'b' }, { title: '字段C', type: 'image', id: 'c' }]
+interface FieldType {
+  _id: string
+  name: string
+}
+
+const icon = <CheckBoxOutlineBlankIcon fontSize='small' />
+const checkedIcon = <CheckBoxIcon fontSize='small' />
+
+const data2 = [{ title: '字段a', _id: 'a' }, { title: '字段B', type: 'number', _id: 'b' }, { title: '字段C', type: 'image', _id: 'c' }]
 
 const Template: React.FC = () => {
   const client = useApolloClient()
-  const [open, setOpen] = useState(false)
+  const [addFieldOpen, setAddFieldOpen] = useState(false)
+  const [fieldsData, setFieldsData] = useState<any[]>([])
+  const [options, setOptions] = useState<readonly FieldType[]>([])
   const { enqueueSnackbar } = useSnackbar()
   const { id } = useParams<{ id: string }>()
   const { loading, error, data } = useQuery(GET_DATA, { variables: { id } })
@@ -47,7 +61,7 @@ const Template: React.FC = () => {
         <List>
           {data2.map(it => (
             <ListItem
-              key={it.id}
+              key={it._id}
               secondaryAction={
                 <IconButton edge='end' onClick={() => { /* TODO: 删除模板字段 */ }}>
                   <DeleteIcon />
@@ -61,40 +75,57 @@ const Template: React.FC = () => {
           ))}
         </List>
       </Card>
-      <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>添加字段</DialogTitle>
+      <Dialog
+        open={addFieldOpen}
+        onClose={() => setAddFieldOpen(false)}
+      >
+        <DialogTitle>添加新字段</DialogTitle>
         <DialogContent>
-          <DialogContentText>请在下面输入新管理员用户的名字和密码</DialogContentText>
-          <TextField
-            autoFocus
-            fullWidth
-            margin='dense'
-            variant='standard'
-            label='用户名'
-            onChange={e => (addUserData.username = e.target.value)}
-          />
-          <TextField
-            autoFocus
-            fullWidth
-            margin='dense'
-            variant='standard'
-            label='密码'
-            type='password'
-            onChange={e => (addUserData.password = e.target.value)}
+          <DialogContentText>请选择需要添加的字段</DialogContentText>
+          <Autocomplete
+            multiple
+            isOptionEqualToValue={(a: any, b: any) => a._id === b._id}
+            getOptionLabel={(option: FieldType) => option.name}
+            options={options}
+            loading={!options.length}
+            limitTags={10}
+            disableCloseOnSelect
+            style={{ minWidth: 300, marginTop: 8 }}
+            renderOption={(props, option: FieldType, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.name}
+              </li>
+            )}
+            value={fieldsData}
+            onChange={(_, value) => setFieldsData(value)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label='请选择需要添加的字段'
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <>
+                      {options.length ? null : <CircularProgress color='inherit' size={20} />}
+                      {params.InputProps.endAdornment}
+                    </>
+                  )
+                }}
+              />
+            )}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>取消</Button>
           <Button
             onClick={() => {
-              setOpen(false)
-              client.query({ query: ADD_USER, variables: addUserData }).then(it => {
-                if (it.error) throw it.error
-                enqueueSnackbar('添加用户成功!', { variant: 'success' })
-              }).catch(e => {
-                console.error(e)
-                enqueueSnackbar('添加用户失败!', { variant: 'error' })
-              })
+              setAddFieldOpen(false)
+              // TODO: 添加字段信息
             }}
           >
             确定
@@ -105,7 +136,7 @@ const Template: React.FC = () => {
         color='primary'
         aria-label='add'
         sx={{ position: 'fixed', bottom: 36, right: 36 }}
-        onClick={() => setOpen(true)}
+        onClick={() => setAddFieldOpen(true)}
       >
         <AddIcon />
       </Fab>
