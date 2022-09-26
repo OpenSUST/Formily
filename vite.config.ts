d3a48aba9ec5da69e4a14921e7a0940f0c249d71
Shirasawa <764798966@qq.com>
2022-09-26 14:47:36 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { existsSync, writeFileSync } from 'fs'
+
+if (!existsSync('config.json')) {
+  writeFileSync('config.json', JSON.stringify({
+    color: 'red',
+    name: '脆弱铅釉陶文物数据库',
+    userMenuName: '铅釉陶',
+    cover: ''
+  }, null, 2))
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
