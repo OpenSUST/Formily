c580afc863ac23988e0ca07e6beec8e3271084b3
Shirasawa <764798966@qq.com>
2022-10-26 22:09:54 +0800

    Add event

--- src/components/fields/Chart.tsx
+++ src/components/fields/Chart.tsx
@@ -11,20 +11,22 @@ const components: Field<string> = {
   getDefaultValue () { return '' },
   isEmpty (value) { return !value.length },
   ViewComponent ({ value, meta }) {
-    const option = useMemo(() => {
+    const [option, events] = useMemo(() => {
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      const events: Record<string, Function> = { }
       try {
-        return meta.extraData
+        return [meta.extraData
           // eslint-disable-next-line no-eval
-          ? eval(`(function (data) { ${meta.extraData.includes('return') ? meta.extraData : 'return ' + meta.extraData} })`)(value.replace(/\r/g, '').split('\n').map(it => it.split(',')))
-          : JSON.parse(value.endsWith(',') ? value.slice(0, -1) : value)
+          ? eval(`(function (data, events) { ${meta.extraData.includes('return') ? meta.extraData : 'return ' + meta.extraData} })`)(value.replace(/\r/g, '').split('\n').map(it => it.split(',')), events)
+          : JSON.parse(value.endsWith(',') ? value.slice(0, -1) : value), events]
       } catch (err) {
         console.error(err)
-        return {}
+        return [{}, events]
       }
     }, [value, meta])
     return (
       <div style={{ width: '100%' }}>
-        <ReactECharts option={option} />
+        <ReactECharts option={option} onEvents={events} />
       </div>
     )
   },
