7ed634c0b6886f870a7a212116a9524adf7a1359
Shirasawa <764798966@qq.com>
2022-10-27 16:35:30 +0800

    Update

--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -4,10 +4,11 @@ import number from './Number'
 import image from './Image'
 import liveOffice from './LiveOffice'
 import chart from './Chart'
+import multiCharts from './MultiCharts'
 import csv from './CSV'
 import multiCSV from './MultiCSV'
 import file from './File'
 import title from './Title'
 
-const obj = { text, number, image, liveOffice, csv, multiCSV, file, title, chart }
+const obj = { text, number, image, liveOffice, csv, multiCSV, file, title, chart, multiCharts }
 export default obj as typeof obj & { [key: string]: Field<any> }
